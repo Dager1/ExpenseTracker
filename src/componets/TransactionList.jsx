1f825826244eac6ead6ae7f20407e4819ca753db
f <franklinrohan2002@gmail.com>
2023-06-06 21:56:36 +0530

    chore:money formater

--- src/componets/TransactionList.jsx
+++ src/componets/TransactionList.jsx
@@ -1,19 +1,39 @@
 import React, { useContext } from "react";
-import { GlobalConstext } from "../Context/GlobalState";
-import Transaction from "./Transaction";
+import { GlobalContext } from "../Context/GlobalState";
 
-function TransactionList() {
-  const { transactions } = useContext(GlobalConstext);
+//Money formatter function
+function moneyFormatter(num) {
+  let p = num.toFixed(2).split(".");
   return (
-    <div>
-      <h3>History</h3>
-      <ul className="list">
-        {transactions.map((transaction) => (
-          <Transaction key={transaction.id} transaction={transaction} />
-        ))}
-      </ul>
-    </div>
+    "$ " +
+    p[0]
+      .split("")
+      .reverse()
+      .reduce(function (acc, num, i, orig) {
+        return num === "-" ? acc : num + (i && !(i % 3) ? "," : "") + acc;
+      }, "") +
+    "." +
+    p[1]
   );
 }
 
-export default TransactionList;
+export const Transaction = ({ transaction }) => {
+  const { deleteTransaction } = useContext(GlobalContext);
+
+  const sign = transaction.amount < 0 ? "-" : "+";
+
+  return (
+    <li className={transaction.amount < 0 ? "minus" : "plus"}>
+      {transaction.text}{" "}
+      <span>
+        {sign}
+        {moneyFormatter(transaction.amount)}
+      </span>
+      <button
+        onClick={() => deleteTransaction(transaction.id)}
+        className="delete-btn">
+        x
+      </button>
+    </li>
+  );
+};
