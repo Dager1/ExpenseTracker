702805ea17fadaeaff93539cbadbfe0b2eb4e5a6
f <franklinrohan2002@gmail.com>
2023-06-06 21:03:32 +0530

    refactored:added js to handel the totaling of amount

--- src/componets/Balance.jsx
+++ src/componets/Balance.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalConstext } from "../Context/GlobalState";
+const { transactions } = useContext(GlobalConstext);
+
+const amounts = transactions.map((transaction) => transaction.amount);
+const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
 
 function Balance() {
   return (
     <div>
       <h4>Your Balance</h4>
-      <h1 id="balance">$0.00</h1>
+      <h1>${total}</h1>
     </div>
   );
 }
