da1c3b6b3234b7ad8433c2f3b2696a7a126b4cbc
f <franklinrohan2002@gmail.com>
2023-06-06 21:06:33 +0530

    refactored:indentation of code

--- src/componets/Balance.jsx
+++ src/componets/Balance.jsx
@@ -1,11 +1,11 @@
 import React, { useContext } from "react";
 import { GlobalConstext } from "../Context/GlobalState";
-const { transactions } = useContext(GlobalConstext);
-
-const amounts = transactions.map((transaction) => transaction.amount);
-const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
 
 function Balance() {
+  const { transactions } = useContext(GlobalConstext);
+
+  const amounts = transactions.map((transaction) => transaction.amount);
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
   return (
     <div>
       <h4>Your Balance</h4>
