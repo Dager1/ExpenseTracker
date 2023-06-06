534a1a252cb58d519d6a23a0ec4e093e66cbdb86
f <franklinrohan2002@gmail.com>
2023-06-06 21:49:07 +0530

    chore:to handel adding of a transactio

--- src/Context/AppReducer.jsx
+++ src/Context/AppReducer.jsx
@@ -1,6 +1,6 @@
 export default (state, action) => {
   switch (action.type) {
-    default:
+    case "DELETE_TRANSACTION":
       return {
         ...state,
         transactions: state.transactions.filter(
@@ -12,5 +12,7 @@ export default (state, action) => {
         ...state,
         transactions: [action.payload, ...state.transactions],
       };
+    default:
+      return state;
   }
 };
